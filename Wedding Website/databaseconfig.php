0a550e7a231d1afa737fc736d7a6c0a064d0b004
Zoran <117390903+PHPZoran@users.noreply.github.com>
2024-05-30 15:06:06 -0400

    Delete Wedding Website directory

--- Wedding Website/databaseconfig.php
+++
@@ -1,7 +0,0 @@
-<?php
-$ini = parse_ini_file('ini/database.ini');
-$con=mysqli_connect($ini['db_ip'],$ini['db_username'],$ini['db_password'],$ini['db_name']);
-if(mysqli_connect_errno()){
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-
